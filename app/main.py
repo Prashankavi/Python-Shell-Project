4a03e1dcb1c94bd87166de889e9bcc3be22c90f3
Prashanth <vjkmrkavitha@gmail.com>
2025-01-08 20:28:52 +0530

    [first shell prompt]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
-
-
+    #input()
+    cmd=input()
+    print(f"{cmd}: command not found")
+    
 if __name__ == "__main__":
     main()
