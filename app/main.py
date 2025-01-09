777f364998904b7ec249f10552eebeec7f4a1255
Prashanth <vjkmrkavitha@gmail.com>
2025-01-09 14:03:59 +0530

    Next Update

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def main():
             else:
                 print(f'{val} not found')
         else:
-            print(f"{cmd}: command not found")
+            print(f"{cmd[0]}: command not found")
 
 if __name__ == "__main__":
     main()
