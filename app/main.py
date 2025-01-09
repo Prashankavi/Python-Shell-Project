441fef64e821cd19620153d95bc47d2ffa035356
Prashanth <vjkmrkavitha@gmail.com>
2025-01-09 13:43:50 +0530

    Next Update

--- app/main.py
+++ app/main.py
@@ -11,6 +11,13 @@ def main():
             sys.exit(0)
         elif cmd.startswith("echo "):
             print(cmd[len("echo "):])
+        elif cmd.startswith("type "):
+            if cmd[len("type "):]=="echo":
+                print("echo is a shell builtin")
+            elif cmd[len("type "):]=="exit":
+                print("exit is a shell builtin")
+            else:
+                print(f"{cmd[len('type '):]}: not found")
         else:
             print(f"{cmd}: command not found")
 
