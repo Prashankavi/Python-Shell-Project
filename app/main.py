0446ba38a61db042a75020dbf9bbd5762dd1f5e8
Prashanth <vjkmrkavitha@gmail.com>
2025-01-11 14:32:20 +0530

    Next Update

--- app/main.py
+++ app/main.py
@@ -1,39 +1,44 @@
 import sys
 import os
-def find_in_path(param):
-    path = os.environ['PATH']
-    print("Path: " + path)
-    print(f"Param: {param}")
-    for directory in path.split(":"):
-        for (dirpath, dirnames, filenames) in os.walk(directory):
-            if param in filenames:
-                return f"{dirpath}/{param}"
-    return None
 def main():
+    path=os.environ.get("PATH")        
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        # Wait for user input
-        command = input()
-        match command.split(" "):
-            case ["exit", "0"]:
-                exit(0)
-            case ["echo", *cmd]:
-                print(" ".join(cmd))
-            case ["type", *cmd]:
-                match cmd:
-                    case ["echo" | "exit" | "type"]:
-                        print(f"${cmd[0]} is a shell builtin")
-                    case _:
-                        location = find_in_path(cmd[0])
-                        if location:
-                            print(f"${cmd[0]} is {location}")
-                        else:
-                            print(f"${" ".join(cmd)} not found")
-            case _:
-                if os.path.isfile(command.split(" ")[0]):
-                    os.system(command)
-                else:
-                    print(f"{command}: command not found")
+        cmd=input().split(" ")
+        command=cmd[0]
+        if command == "exit":
+            sys.exit(0)
+        elif command=="echo":
+            print(" ".join(cmd[1:]))
+        elif command=="type":
+            val=cmd[1]
+            cmdpath=None
+            paths=path.split(":")
+            for i in paths:
+                if os.path.exists(f"{i}/{val}"):
+                    cmdpath=f"{i}/{val}"
+            if val in ["echo","exit","type"]:
+                print(f'{val} is a shell builtin')
+            elif cmdpath:
+                print(f"{val} is {cmdpath}\n")
+            else:
+                print(f'{val} not found')
+        else:
+            #print(f"{cmd[0]}: command not found")
+            executable_path=None
+            paths=path.split(":")
+            for directory in paths:
+                potential_path=os.path.join(directory, command)
+                if os.access(potential_path, os.X_OK):
+                    executable_path = potential_path
+                    break
+            if not executable_path and os.access(command, os.X_OK):
+                executable_path = os.path.abspath(command)
+            if executable_path:
+                os.system(" ".join(cmd))
+            else:
+                print(f"{command}: command not found")
+
 if __name__ == "__main__":
     main()
