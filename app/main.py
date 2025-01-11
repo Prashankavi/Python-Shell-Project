d4f9508cd13e11666d7171a8eaff86aef89e8429
Prashanth <vjkmrkavitha@gmail.com>
2025-01-11 14:24:14 +0530

    Next Update

--- app/main.py
+++ app/main.py
@@ -8,22 +8,34 @@ def locate_executable(command) -> Optional[str]:
         file_path = os.path.join(directory, command)
         if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
             return file_path
+
 def handle_exit(args):
     sys.exit(int(args[0]) if args else 0)
+
 def handle_echo(args):
     print(" ".join(args))
+
 def handle_type(args):
     if args[0] in builtins:
         print(f"{args[0]} is a shell builtin")
-    elif executable := locate_executable(args[0]):
-        print(f"{args[0]} is {executable}")
+    elif executable := locate_executable(command):
+
+        # Generate and print program signature
+        #program_signature = generate_program_signature(command, args)
+        #print(f"Program Signature: {program_signature}")
+
+        # Execute the command
+        subprocess.run([executable, *args])
+
     else:
         print(f"{args[0]} not found")
+
 builtins = {
     "exit": handle_exit,
     "echo": handle_echo,
     "type": handle_type,
 }
+
 def main():
     while True:
         sys.stdout.write("$ ")
@@ -33,9 +45,10 @@ def main():
             builtins[command](args)
             continue
         elif executable := locate_executable(command):
-            subprocess.run([executable, *args])
+            subprocess.run([command, *args])
         else:
             print(f"{command}: command not found")
         sys.stdout.flush()
+
 if __name__ == "__main__":
     main()
